4dc1c49b4b96233841800c492ef18202841273a9
James Kozianski <soulplant@gmail.com>
2014-06-12 10:30:16 +1000

    Initial commit.

---
+++ desktop/src/com/matchgame/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.matchgame.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.matchgame.MatchGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MatchGame(), config);
+	}
+}
