2f50f078e0018849dcdc7a91256630dc52df4f56
James Kozianski <soulplant@gmail.com>
2014-07-01 10:42:56 +1000

    Fix deprecation warning from FreeTypeFontGenerator, reformat MatchGame.java.

--- core/src/com/matchgame/MatchGame.java
+++ core/src/com/matchgame/MatchGame.java
@@ -11,47 +11,50 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator.FreeTypeFontParameter;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
 public class MatchGame extends ApplicationAdapter {
-	SpriteBatch batch;
+  SpriteBatch batch;
   private Stage stage;
   private final List<Texture> blockTextures = new ArrayList<Texture>();
   private DefaultBlockFactory blockFactory;
   private CyclicPhaseRunner runner;
 
-	@SuppressWarnings("deprecation")
   @Override
-	public void create () {
-	  FreeTypeFontGenerator gen = new FreeTypeFontGenerator(Gdx.files.internal("JosefinSlab-Regular.ttf"));
-	  BitmapFont font = gen.generateFont(100);
-	  gen.dispose();
-		batch = new SpriteBatch();
-		for (int i = 0; i < 4; i++) {
-		  blockTextures.add(new Texture("Block" + (i + 1) + ".png"));
-		}
-		blockFactory = new DefaultBlockFactory(new Random(), blockTextures);
-		stage = new Stage(new ScreenViewport());
-		Gdx.input.setInputProcessor(stage);
-		List<Phase> phases = new ArrayList<Phase>();
-		phases.add(new IntroPhase(blockFactory, stage));
-		phases.add(new GamePhase(blockFactory, stage, font));
-		runner = new CyclicPhaseRunner(phases);
-	}
+  public void create() {
+    FreeTypeFontGenerator gen = new FreeTypeFontGenerator(
+        Gdx.files.internal("JosefinSlab-Regular.ttf"));
+    FreeTypeFontParameter param = new FreeTypeFontParameter();
+    param.size = 150;
+    BitmapFont font = gen.generateFont(param);
+    gen.dispose();
+    batch = new SpriteBatch();
+    for (int i = 0; i < 4; i++) {
+      blockTextures.add(new Texture("Block" + (i + 1) + ".png"));
+    }
+    blockFactory = new DefaultBlockFactory(new Random(), blockTextures);
+    stage = new Stage(new ScreenViewport());
+    Gdx.input.setInputProcessor(stage);
+    List<Phase> phases = new ArrayList<Phase>();
+    phases.add(new IntroPhase(blockFactory, stage));
+    phases.add(new GamePhase(blockFactory, stage, font));
+    runner = new CyclicPhaseRunner(phases);
+  }
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 1, 1, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+  @Override
+  public void render() {
+    Gdx.gl.glClearColor(1, 1, 1, 1);
+    Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
     Gdx.gl.glEnable(GL20.GL_BLEND);
     Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
     runner.act(Gdx.graphics.getDeltaTime());
     stage.draw();
-	}
+  }
 
-	@Override
-	public void dispose() {
-	  stage.dispose();
-	}
+  @Override
+  public void dispose() {
+    stage.dispose();
+  }
 }
